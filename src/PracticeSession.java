754ea55d10b39fea7d0214774a85a9c0f4898cf8
Lewis Ledesma <lewis.ledesma@gmail.com>
2021-05-18 12:50:57 -0400

    Uploaded a test file with two functions to modify

---
+++ src/PracticeSession.java
@@ -0,0 +1,14 @@
+public class PracticeSession {
+    public static void main(String[] args) {
+        feature_1();
+        feature_2();
+    }
+
+    public static void feature_1(){
+        System.out.println("Feature 1");
+    }
+
+    public static void feature_2(){
+        System.out.println("Feature 2");
+    }
+}
