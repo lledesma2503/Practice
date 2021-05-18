9b8777d57790592d2ddc1cf8cc77f8a49a3d3c4c
Lewis Ledesma <lewis.ledesma@gmail.com>
2021-05-18 13:00:08 -0400

    Changed Feature 1 text to "Changed Feature 1"

--- src/PracticeSession.java
+++ src/PracticeSession.java
@@ -5,7 +5,7 @@ public class PracticeSession {
     }
 
     public static void feature_1(){
-        System.out.println("Feature 1");
+        System.out.println("Changed Feature 1");
     }
 
     public static void feature_2(){
